a7bdda1f05ba783a234f6a34f4907c505d09a1a0
Chong Vang <vangcguy@gmail.com>
2018-05-10 07:12:29 -0500

    message

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ alert("This application will calculate your pay for the week.");
 
 let numberOne= prompt("Please enter hours worked",0);
 let numberTwo= prompt("Enter pay rate",0);
-let sum = numberOne * numberTwo;
-console.log(sum);
-alert("You made " + sum);
+
+if(numberOne>40){
+	let numberThree = 40 * numberTwo;
+	let numberFour = (numberOne - 40) * (numberTwo * 1.5);
+	console.log(numberThree + numberFour);
+	console.log("")
+}
+else{
+	console.log(numberOne * numberTwo);
+}
+
+
+console.log(numberOne);
+
+console.log(numberTwo);
